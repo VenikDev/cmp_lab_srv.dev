a74635a437363efda8afc1536bb1cb4712ebe3f4
VenikDev <sashavisimsasha@gmail.com>
2023-02-23 13:35:49 +0500

    delete test data

--- server/src/model/models.go
+++ server/src/model/models.go
@@ -1,14 +1,1 @@
 package model
-
-type Album struct {
-	ID     uint64 `json:"ID"`
-	Name   string `json:"name"`
-	Rating uint8  `json:"rating"`
-	Author string `json:"author"`
-}
-
-var Albums = [...]Album{
-	{ID: 1, Name: "Rammstein", Rating: 5, Author: "Rammstein"},
-	{ID: 2, Name: "Raize Raize", Rating: 5, Author: "Rammstein"},
-	{ID: 3, Name: "Main tail", Rating: 5, Author: "Rammstein"},
-}
