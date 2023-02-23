9b4442521985dfb10381395e895cf2f7b382f63c
VenikDev <sashavisimsasha@gmail.com>
2023-02-23 13:33:06 +0500

    Add config

--- server/src/transport/route.go
+++ server/src/transport/route.go
@@ -2,6 +2,7 @@ package transport
 
 import (
 	"comparisonLaboratories/src/model"
+	"comparisonLaboratories/src/services"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -13,34 +14,42 @@ func SetupRouters(app *gin.Engine) {
 		context.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	app.GET("/api/v1/album/:id", func(context *gin.Context) {
-		id, err := strconv.Atoi(context.Param("id"))
-		if err != nil {
-			log.Fatalln("id album isn int")
-			return
-		}
-		for _, a := range model.Albums {
-			if a.ID == uint64(id) {
-				context.IndentedJSON(http.StatusOK, a)
+	app.GET(API_V1+"/album/:id", func(context *gin.Context) {
+		paramID := context.Param("id")
+
+		if paramID != "" {
+			id, err := strconv.Atoi(paramID)
+			if err != nil {
+				log.Fatalln("id album isn int")
 				return
+			}
+			for _, a := range model.Albums {
+				if a.ID == uint64(id) {
+					context.IndentedJSON(http.StatusOK, a)
+					return
+				}
 			}
 		}
 
-		context.IndentedJSON(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		context.IndentedJSON(
+			http.StatusNotFound,
+			http.StatusText(http.StatusNotFound))
 	})
 
-	app.GET("/api/v1/albums", func(context *gin.Context) {
+	app.GET(API_V1+"/albums", func(context *gin.Context) {
 		context.IndentedJSON(http.StatusOK, model.Albums)
 	})
 
-	app.POST("/api/v1/analysis", func(context *gin.Context) {
+	app.POST(API_V1+"/analysis", func(context *gin.Context) {
 		url := context.Query("url")
 
-		result := parse.Parse(url)
-		if result != nil {
-			context.IndentedJSON(http.StatusOK, result)
-		} else {
-			context.IndentedJSON(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		if url != "" {
+			result, err := services.GetAnalysis(url)
+			if err == nil {
+				context.IndentedJSON(http.StatusOK, result)
+			} else {
+				context.IndentedJSON(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+			}
 		}
 	})
 }
---
+++ server/src/transport/url.go
@@ -0,0 +1,5 @@
+package transport
+
+const (
+	API_V1 = "/api/v1"
+)
