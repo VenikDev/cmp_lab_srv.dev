b0c3249085a703f1e72c148e951050dc2b8f7a5d
VenikDev <sashavisimsasha@gmail.com>
2023-02-26 21:07:32 +0500

    - Small fixes

--- client/src/components/common/translate.js
+++ client/src/components/common/translate.js
@@ -1,11 +1,11 @@
 import * as keys from "../../assets/dict/key_word.json";
 
-
 /**
  *
  * @param searchKey {string} - ключ
  * @constructor
  */
 export function Translate(searchKey) {
+  console.log("key = ", searchKey, ", value ", keys[searchKey])
   return keys[searchKey]
 }
