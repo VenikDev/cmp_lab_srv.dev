9e4cb5ba8b0cdf8e8aa71d7e9e64615f11db2d56
VenikDev <sashavisimsasha@gmail.com>
2023-03-05 20:01:36 +0500

    - add route handlers

--- server/src/transport/route.go
+++ server/src/transport/route.go
@@ -1,36 +1,11 @@
 package transport
 
 import (
-	"comparisonLaboratories/src/clog"
-	"comparisonLaboratories/src/core"
-	"comparisonLaboratories/src/services"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
 )
 
 func InitRouters(app *gin.Engine) {
-	app.GET("/", func(context *gin.Context) {
-		context.HTML(http.StatusOK, "index.html", nil)
-	})
-
-	app.GET(API_V1+"/get_labs", func(context *gin.Context) {
-		context.IndentedJSON(http.StatusOK, core.Laboratories)
-	})
-
-	app.GET(API_V1+"/analysis", func(context *gin.Context) {
-		key := context.Query("key")
-		strings.Trim(key, " ")
-		clog.Logger.Info("InitRouters", "key word", key)
-
-		if key != "" {
-			result, err := services.GetLaboratoryAnalyses(key)
-			if err == nil {
-				context.IndentedJSON(http.StatusOK, result)
-				return
-			}
-		}
-
-		context.IndentedJSON(http.StatusNotFound, http.StatusText(http.StatusNotFound))
-	})
+	app.GET("/", GetIndexHtml)
+	app.GET(API_V1+"/get_labs", GetLabs)
+	app.GET(API_V1+"/analysis", GetListAnalyses)
 }
