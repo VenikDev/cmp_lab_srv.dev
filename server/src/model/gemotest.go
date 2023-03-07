44571a5c6b23fa255c8530870b330563b9badff6
VenikDev <sashavisimsasha@gmail.com>
2023-03-07 21:12:56 +0500

    - fix

--- server/src/model/gemotest.go
+++ server/src/model/gemotest.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"comparisonLaboratories/src/algorithm"
+	"comparisonLaboratories/src/global"
+	"comparisonLaboratories/src/herr"
 	"github.com/PuerkitoBio/goquery"
 	"regexp"
 	"strconv"
@@ -10,20 +13,40 @@ func GetAnalyzesGemotest(document *goquery.Document) ListAnalyses {
 	result := make(ListAnalyses, 0)
 	re := regexp.MustCompile("[0-9]+")
 
-	// Find the review items
+	// This code is using the Go programming language and the goquery library to scrape data from an HTML document.
+	// It is finding all elements with the class "analize-item_narrow" and then running a function for each of them.
+	// For each element, it is finding the text of an element with the class "analize-item__title" inside it,
+	// which represents the title of a product.
+	// It is also finding the text of an element with the class "add-to-cart__price",
+	// which represents the price of that product.
+	// If both the title and price are not empty,
+	// it is using a regular expression to extract the numerical value from the price string,
+	// converting it to an integer, and then appending the title and price to a result slice of Analysis structs.
 	document.Find(".analize-item_narrow").Each(func(i int, selection *goquery.Selection) {
 		// For each item found, get the title
-		title := selection.Find(".analize-item__title").Find("a").Text()
+		tagA := selection.Find(".analize-item__title").Find("a")
+		title := tagA.Text()
 		if title != "" {
 			price := selection.Find(".add-to-cart__price").Text()
 
 			// если не пустые
 			if price != "" {
+				linkToAnalyses, _ := tagA.Attr("href")
+
 				totalPrice, err := strconv.Atoi(re.FindString(price))
+				herr.HandlerError(err, "Not parse price")
 				if err == nil {
+					idx := algorithm.LinearSearch(global.Laboratories, func(lab global.Laboratory) bool {
+						if lab.Name == GEMOTEST {
+							return true
+						}
+						return false
+					})
 					result = append(result, Analysis{
-						Name:  title,
-						Price: totalPrice,
+						Name:        title,
+						Price:       totalPrice,
+						Description: "",
+						OriginalURL: global.Laboratories[idx].Url + linkToAnalyses,
 					})
 				}
 			}
--- server/src/model/invitro.go
+++ server/src/model/invitro.go
@@ -1,9 +1,13 @@
 package model
 
 import (
+	"comparisonLaboratories/src/algorithm"
+	"comparisonLaboratories/src/global"
+	"comparisonLaboratories/src/herr"
 	"github.com/PuerkitoBio/goquery"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func GetAnalyzesInvitro(document *goquery.Document) ListAnalyses {
@@ -14,17 +18,35 @@ func GetAnalyzesInvitro(document *goquery.Document) ListAnalyses {
 	document.Find(".iwg_margin").Each(func(i int,
 		selection *goquery.Selection) {
 		// For each item found, get the title
-		title := selection.Find("a").Find("h3").Text()
+		tagWithTitle := selection.Find("a")
+		title := tagWithTitle.Find("h3").Text()
 		if title != "" {
+			linkToAnalyses, _ := tagWithTitle.Attr("href")
 			price := selection.Find(".search__result-order-meta-price ").Text()
 
+			// The first line of code selects the text content of the `<p>` element within an HTML element with class
+			// "search__result" using the Go package "selection". The result is assigned to the variable "description".
+			// The second line of code trims any leading and trailing spaces from the "description" string using the
+			// Go package "strings".
+			description := selection.Find(".search__result").Find("p").Text()
+			strings.Trim(description, " ")
 			// если не пустые
 			if price != "" {
 				totalPrice, err := strconv.Atoi(re.FindString(price))
+				herr.HandlerError(err, "Not parse price")
 				if err == nil {
+					idx := algorithm.LinearSearch(global.Laboratories, func(lab global.Laboratory) bool {
+						if lab.Name == INVITRO {
+							return true
+						}
+						return false
+					})
+
 					result = append(result, Analysis{
-						Name:  title,
-						Price: totalPrice,
+						Name:        title,
+						Price:       totalPrice,
+						Description: description,
+						OriginalURL: global.Laboratories[idx].Url + linkToAnalyses,
 					})
 				}
 			}
