cbdb144d68c3ea57a9c255f222942e6a254725a3
Alexander <a.oshurkov@iridi.tech>
2023-03-17 15:17:21 +0500

    - changes in structure of project

--- client_v2/src/modules/nav-bar/nav-bar.tsx
+++ client_v2/src/modules/nav-bar/nav-bar.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import classes from "./style.module.css";
 
 const NavBar = () => {
   return (
-    <div className={classes.navBar}>
-
-    </div>
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <img className="h-8 w-8" src="/logo.svg" alt="Logo" />
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <a href="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+              <a href="/products" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Products</a>
+              <a href="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
-};
+}
 
 export default NavBar;
--- client_v2/src/pages/main/App.tsx
+++ client_v2/src/pages/main/App.tsx
@@ -2,11 +2,13 @@ import React, { useRef } from "react";
 import classes from './App.module.css'
 import {CButton} from '../../ui/button/button'
 import {CInput} from "../../ui/input/input";
+import NavBar from "../../modules/nav-bar/nav-bar";
 
 function App() {
 
   return (
     <div className={classes.App}>
+      <NavBar/>
       <h2>
         React icon
       </h2>
