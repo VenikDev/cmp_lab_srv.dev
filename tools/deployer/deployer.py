b2f34d07f8f712c193799dd6384ded860519c344
VenikDev <sashavisimsasha@gmail.com>
2023-05-21 18:03:31 +0500

    - fix docker-compose.yml

---
+++ tools/deployer/deployer.py
@@ -0,0 +1,30 @@
+import paramiko
+from dotenv import load_dotenv
+import os
+
+
+def main():
+    ssh = paramiko.SSHClient()
+
+    # добавляем новый ключ в список известных хостов
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # устанавливаем соединение
+    ssh.connect(
+        os.getenv('SSH_IP'),
+        username=os.getenv('SSH_NAME'),
+        password=os.getenv('SSH_PASSWORD'))
+
+    # отправляем команду
+    ssh.exec_command('cd cmp-srv/')
+    ssh.exec_command('git pull')
+    stdin, stdout, stderr = ssh.exec_command('make deploy')
+
+    result = stdout.read()
+    print(result.decode())
+    ssh.close()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
